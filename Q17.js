57a52420a470f73cfc84f6e6ee023f5bf8425e5e
PrathamSahu7777 <139504189+PrathamSahu7777@users.noreply.github.com>
2024-05-08 23:41:00 +0530

    Create Q17.js

---
+++ Q17.js
@@ -0,0 +1,6 @@
+function splitStringIntoWords(str) {
+return str.split(" ").filter(word => word !== '');
+}
+const sentence = "Splitting the sentence.";
+console.log(splitStringIntoWords(sentence));
+//output: [ 'Splitting', 'the', 'sentence.' ]
