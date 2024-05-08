f5a59ac3a1b8aae27833c1bb07a9be06c3672a45
PrathamSahu7777 <139504189+PrathamSahu7777@users.noreply.github.com>
2024-05-08 23:49:36 +0530

    Create Q_18.js

---
+++ Q_18.js
@@ -0,0 +1,12 @@
+function convertCase(str) {
+return str.replace(/[a-z]/ig, function(match) {
+return match === match.toLowerCase() ? 
+match.toUpperCase() : match.toLowerCase();
+});
+}
+const upperCase= "uppercase";
+const lowerCase="LOWERCASE";
+console.log("Converted string:", convertCase(upperCase));
+console.log("Converted string:", convertCase(lowerCase));
+//output:Converted string: UPPERCASE
+//       Converted string: lowercase
