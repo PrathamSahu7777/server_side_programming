a785873a68ac97b1e0bda16bf55bb897d71d4b7e
PrathamSahu7777 <139504189+PrathamSahu7777@users.noreply.github.com>
2024-05-08 23:55:08 +0530

    Create Q_21.js

---
+++ Q_21.js
@@ -0,0 +1,17 @@
+class Node {
+constructor(data) {
+this.data = data;
+this.next = null;
+}
+}
+class LinkedList {
+constructor() {
+this.head = null;
+}
+isEmpty() {
+return this.head === null;
+}
+}
+const list = new LinkedList();
+console.log("Is the list empty?", list.isEmpty());
+//output: Is the list empty? true
