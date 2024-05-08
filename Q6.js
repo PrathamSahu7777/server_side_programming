f06b8525f2ad3bc8381ab68f8c81ebb69b0d19af
PrathamSahu7777 <139504189+PrathamSahu7777@users.noreply.github.com>
2024-05-08 23:22:55 +0530

    Create Q6.js

---
+++ Q6.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+for (let i = 1; i < arr.length; i++) {
+let current = arr[i];
+let j = i - 1;
+while (j >= 0 && arr[j] > current) {
+arr[j + 1] = arr[j];
+j--;
+}
+arr[j + 1] = current;
+}
+return arr;
+}
+const unsortedArray = [5, 3, 7, 2, 9, 1, 6, 4, 8];
+const sortedArray = insertionSort(unsortedArray);
+console.log("Sorted array :", sortedArray);
+//output:Sorted array: [1, 2, 3, 4, 5,6, 7, 8, 9]
